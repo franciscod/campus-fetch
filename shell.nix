a96fbd919b82d6c42531aed1e1a970c6610322d1
Francisco Demartino <git@fd1.com.ar>
2023-12-13 22:29:30 -0300

    add Nix support without virtualenv

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+  pkgs.mkShell {
+    packages = [
+      pkgs.python3Packages.html2text
+      pkgs.python3Packages.beautifulsoup4
+      pkgs.python3Packages.requests
+      pkgs.python3Packages.unidecode
+    ];
+  }
